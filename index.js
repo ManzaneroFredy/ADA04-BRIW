cb1b31363d0d5692f8d2692ce5e45ee571ca7273
Patish <Patricio.17po.@gmail.com>
2023-10-13 19:42:59 -0600

    funcionalidad carga de archivos y css

--- index.js
+++ index.js
@@ -5,3 +5,28 @@
             console.log(archivos[i].name);
         }
     });
+
+
+    // Mostrar lista de archivos cargados
+    document.getElementById('archivos').addEventListener('change', function() {
+        var listaArchivos = document.getElementById('lista-archivos');
+        listaArchivos.innerHTML = '';
+
+        for(var i = 0; i < this.files.length; i++) {
+            var archivo = this.files[i];
+            var nuevoElemento = document.createElement('div');
+            nuevoElemento.textContent = archivo.name;
+            listaArchivos.appendChild(nuevoElemento);
+        }
+    });
+    
+    //Mostrar el boton cuando se cargan los archivos
+document.getElementById('archivos').addEventListener('change', function() {
+    var archivos = this.files;
+    var subirButton = document.getElementById('subir');
+    if (archivos.length > 0) {
+        subirButton.style.display = 'block';
+    } else {
+        subirButton.style.display = 'none';
+    }
+});
